e42b1b531061d0bfd8d15313a27080a609fcc15b
Puneet Pugalia <rohit14kvs@gmail.com>
2022-04-14 14:13:26 +0530

    Added form state

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,33 +1,138 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 
 const Form = () => {
+  const [state, setState] = useState({
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    category: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
+
+  const [selectGender, setGender] = useState("");
+
+  const gender = ["Male", "Female", "Uncategorized"];
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setState({
+      [name]: value,
+    });
+  };
+
+  const handleGender = (e) => {
+    const value = e.target.value;
+    setGender(value);
+  };
+  console.log(state.firstName);
+  console.log(state.middleName);
+  console.log(state.lastName);
+  console.log(state.DOB);
+  console.log(state.category);
+  console.log(state.email);
+  console.log(state.phone);
+  console.log(state.address);
+  console.log(selectGender);
+
   return (
     <div className="w-full bg-[#F9F9F9] pb-10">
       <h2 className=" uppercase py-10 w-2/3 mx-auto text-lg font-semibold">
         Submit your Application
       </h2>
       <form className=" w-2/3 mx-auto flex flex-col">
-        <Input text={"First Name"} inputName={"First name"} type={"text"} />
-        <Input text={"Middle Name"} inputName={"Middle name"} type={"text"} />
-        <Input text={"Last Name"} inputName={"Last name"} type={"text"} />
-        <Input text={"Date of Birth"} inputName={"date"} type={"date"} />
-        <Input text={"Gender"} inputName={"gender"} type={"radio"} />
-        <Input text={"Category"} inputName={"category"} type={"select"} />
-        <Input text={"Email"} inputName={"email"} type={"email"} />
-        <Input text={"Phone No."} inputName={"Phone"} type={"tel"} />
-        <Input text={"Address"} inputName={"address"} type={"textarea"} />
+        <Input
+          text={"First Name"}
+          type={"text"}
+          name={"firstName"}
+          value={state.firstName}
+          onChanges={handleChange}
+          required={true}
+        />
+        <Input
+          text={"Middle Name"}
+          type={"text"}
+          name={"middleName"}
+          value={state.middleName}
+          onChanges={handleChange}
+          required={false}
+        />
+        <Input
+          text={"Last Name"}
+          type={"text"}
+          name={"lastName"}
+          value={state.lastName}
+          onChanges={handleChange}
+          required={true}
+        />
+        <Input
+          text={"Date of Birth"}
+          type={"date"}
+          name={"DOB"}
+          value={state.DOB}
+          onChanges={handleChange}
+          required={true}
+        />
+        <div className="mr-2 mb-6 text-lg">
+          <label htmlFor={"Gender"}>Gender :</label>
+          {gender.map((g, index) => (
+            <label className="mx-2">
+              <input
+                id="Gender"
+                key={index}
+                type={"radio"}
+                name={"gender"}
+                value={g}
+                checked={selectGender === g}
+                onChange={handleGender}
+                className="mr-1"
+                required
+              />
+              {g}
+            </label>
+          ))}
+        </div>
+        <Input
+          text={"Category"}
+          type={"select"}
+          value={state.category}
+          name={"category"}
+          onChanges={handleChange}
+        />
+        <Input
+          text={"Email"}
+          type={"email"}
+          name={"email"}
+          value={state.email}
+          onChanges={handleChange}
+          required={true}
+        />
+        <Input
+          text={"Phone No."}
+          type={"tel"}
+          name={"phone"}
+          value={state.phone}
+          onChanges={handleChange}
+        />
+        <Input
+          text={"Address"}
+          type={"textarea"}
+          name={"address"}
+          value={state.address}
+          onChanges={handleChange}
+        />
         <Input
           text={"Personal Picture"}
           name={"img"}
-          inputName={"img"}
           type={"file"}
           accept={"image/*"}
         />
         <Input
           text={"Signature Picture"}
           name={"img"}
-          inputName={"img"}
           type={"file"}
           accept={"image/*"}
         />
--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,86 +1,105 @@
 import React from "react";
 
-const Input = ({ text, inputName, type, accept }) => {
+const Input = ({ text, type, accept, value, onChanges, name, required }) => {
   const category = ["Select", "General", "OBC", "SC/ST"];
-  const gender = ["Male", "Female", "Uncategorized"];
   return (
     <div className="mb-6 text-lg">
       {(type === "text" || type === "email" || type === "date") && (
         <div>
-          <label className="mr-2" htmlFor={inputName}>
+          <label className="mr-2" htmlFor={text}>
             {text} :
           </label>
-          <input
-            className="px-2 py-1"
-            type={type}
-            name={inputName}
-            id={inputName}
-            placeholder={inputName}
-          />
-        </div>
-      )}
-      {type === "radio" && (
-        <div>
-          <label className="mr-2" htmlFor={inputName}>
-            {text} :
-          </label>
-          {gender.map((g) => (
-            <>
-              <input className="mr-1" type={type} name={inputName} id={g} />
-              <label className="mr-4" htmlFor={g}>
-                {g}
-              </label>
-            </>
-          ))}
+          {required ? (
+            <input
+              className="px-2 py-1"
+              type={type}
+              name={name}
+              id={text}
+              placeholder={text}
+              value={value}
+              onChange={onChanges}
+              required
+            />
+          ) : (
+            <input
+              className="px-2 py-1"
+              type={type}
+              name={name}
+              id={text}
+              placeholder={text}
+              value={value}
+              onChange={onChanges}
+            />
+          )}
         </div>
       )}
       {type === "tel" && (
         <div>
-          <label className="mr-2" htmlFor={inputName}>
+          <label className="mr-2" htmlFor={text}>
             {text} :
           </label>
           <input
             type={type}
-            name={inputName}
-            id={inputName}
-            placeholder={inputName}
+            name={name}
+            id={text}
+            placeholder={text}
             pattern="[0-9]{10}"
+            value={value}
+            onChange={onChanges}
+            required
           />
         </div>
       )}
       {type === "select" && (
         <div>
-          <label className="mr-2" htmlFor={inputName}>
+          <label className="mr-2" htmlFor={text}>
             {text} :
           </label>
-          <select name={inputName} id={inputName}>
-            {category.map((c) => (
-              <option value={c}>{c}</option>
-            ))}
+          <select
+            name={name}
+            id={text}
+            value={value}
+            onChange={onChanges}
+            required
+          >
+            {category.map((c, index) =>
+              index === 0 ? (
+                <option value="" key={index}>
+                  {c}
+                </option>
+              ) : (
+                <option value={c} key={index}>
+                  {c}
+                </option>
+              )
+            )}
           </select>
         </div>
       )}
       {type === "textarea" && (
         <div className=" flex">
-          <label className="mr-2" htmlFor={inputName}>
+          <label className="mr-2" htmlFor={text}>
             {text} :
           </label>
           <textarea
             className="px-2 py-1"
-            name={inputName}
+            name={name}
             cols="30"
             rows="3"
             placeholder={text}
-            id={inputName}
+            id={text}
+            value={value}
+            onChange={onChanges}
+            required
           ></textarea>
         </div>
       )}
       {type === "file" && (
         <div>
-          <label className="mr-2" htmlFor={inputName}>
+          <label className="mr-2" htmlFor={text}>
             {text} :
           </label>
-          <input type="file" accept={accept} id={inputName} />
+          <input type="file" accept={accept} id={text} required />
         </div>
       )}
     </div>
